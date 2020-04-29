dacc8aa60a76b981165a53301893d94db1144901
thomas <tc.thomaschartier@gmail.com>
2020-04-29 20:04:41 +0200

    change icons from vue config

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,14 @@ module.exports = {
 module.exports = {
   pwa: {
     name:'Kana Learn',
-    icons:{
-      favicon32: 'public/img/icons/pngfuel.com.png',
-      favicon16: 'public/img/icons/pngfuel.com.png',
-      appleTouchIcon: 'public/img/icons/pngfuel.com.png',
-      maskIcon: 'public/img/icons/pngfuel.com.png',
-      msTileImage: 'public/img/icons/pngfuel.com.png'
-    },
+    icons: [    
+        {
+          'src': 'public/img/icons/pngfuel.com.png',
+          'sizes': '192x192',
+          'type': 'image/png',
+      },
+    ]
+    ,
     appleMobileWebAppStatusBarStyle: 'white',
   }
 }
