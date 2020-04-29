29ece90eac14ec8b830554fa1fba6705ece91608
thomas <tc.thomaschartier@gmail.com>
2020-04-29 19:59:24 +0200

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
 module.exports = {
   pwa: {
     name:'Kana Learn',
-    iconPaths:{
+    icons:{
       favicon32: 'public/img/icons/pngfuel.com.png',
       favicon16: 'public/img/icons/pngfuel.com.png',
       appleTouchIcon: 'public/img/icons/pngfuel.com.png',
