35567fc688dae370f4d9486f64b7b97c9844a62f
thomas <tc.thomaschartier@gmail.com>
2020-04-29 19:52:35 +0200

    change logo, test statusbar

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,14 @@ module.exports = {
 
 module.exports = {
   pwa: {
-    name:'Kana Learn'
+    name:'Kana Learn',
+    iconPaths:{
+      favicon32: 'public/img/icons/pngfuel.com.png',
+      favicon16: 'public/img/icons/pngfuel.com.png',
+      appleTouchIcon: 'public/img/icons/pngfuel.com.png',
+      maskIcon: 'public/img/icons/pngfuel.com.png',
+      msTileImage: 'public/img/icons/pngfuel.com.png'
+    },
+    appleMobileWebAppStatusBarStyle: 'white',
   }
 }
