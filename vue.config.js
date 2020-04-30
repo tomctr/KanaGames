def7f85eb580013732e70dc874943e294154c7e7
thomas <tc.thomaschartier@gmail.com>
2020-04-30 13:51:21 +0200

    change manifest json gen

--- vue.config.js
+++ vue.config.js
@@ -6,15 +6,19 @@ module.exports = {
 
 module.exports = {
   pwa: {
-    name:'Kana Learn',
-    icons: [    
+    name: 'Kana Learn',
+    manifestOptions: {
+      name : 'Kana Learn',
+      icons: [
         {
           'src': 'img/icons/logo-196.png',
-          'sizes': '196x196',
+          'sizes': '192x192',
           'type': 'image/png',
-      },
-    ]
+        },
+      ]
+    }
     ,
     appleMobileWebAppStatusBarStyle: 'white',
+    themeColor: '#4DBA87',
   }
 }
