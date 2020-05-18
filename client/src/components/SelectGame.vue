5d9d07421afdf0390d7398b67efb978745c2adee
thomas <tc.thomaschartier@gmail.com>
2020-05-18 21:10:23 +0200

    test change flex

--- client/src/components/SelectGame.vue
+++ client/src/components/SelectGame.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="fill-height d-flex flex-column">
-    <v-card id="cardCategory" v-for="cat in gameCategory" :key="cat" :to="cat.link" outlined tile height="150px" class="ma-5 d-flex align-center justify-center" dark shaped>
+  <div class="fill-height" id="selectDiv">
+    <v-card id="cardCategory" v-for="cat in gameCategory" :key="cat" :to="cat.link" outlined tile height="450px" width="450px"  class="ma-5 d-flex align-center justify-center" dark shaped>
       <span id="textCategory" class="display-2">{{cat.title}}</span>
     </v-card>
   </div>
@@ -21,5 +21,12 @@ export default {
 <style>
 #cardCategory{
   background-color: rgba(55, 57, 86, 0.7);
+  align-self: center;
+
+}
+#selectDiv{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
